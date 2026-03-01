5d4e72dedea31febc81f83f1393870a72bdbd384
rafaelcarlosdossantos-netizen <rafael.carlosdossantos@gmail.com>
2026-03-01 19:27:34 +0000

    Update vite.config.js

--- sistema-limpeza-frontend/vite.config.js
+++ sistema-limpeza-frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()]
-})
+export default {
+  // Configuração mínima para o Vite funcionar na Vercel
+  build: {
+    outDir: 'dist'
+  }
+}
